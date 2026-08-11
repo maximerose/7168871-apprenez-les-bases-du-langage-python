67c1d7b0db985633075429bd38bd3848c49ce0bd
maximerose <maxime.r80@gmail.com>
2026-08-11 18:32:41 +0200

    Exercice P3C3

--- P3/P3C3/énoncé/main.py
+++ P3/P3C3/énoncé/main.py
@@ -1,5 +1,38 @@
-# Écrivez votre code ici !
+import csv
 
+def extract(filename = "input.csv"):
+    data = []
+    with open(filename, "r") as csv_file:
+        reader = csv.DictReader(csv_file, delimiter=',')
+        for line in reader:
+            data.append(line)
+
+    return data
+
+def transform(data_to_transform):
+    data_to_load = []
+
+    for data in data_to_transform:
+        transformed_data = {}
+        transformed_data["nom"] = data["nom"]
+        transformed_data["salaire"] = int(data["heures_travaillees"]) * 15
+        data_to_load.append(transformed_data)
+
+    return data_to_load
+
+def load(data_to_load, filename = "output.csv"):
+    with open(filename, mode="w") as file:
+        fieldnames = ["nom", "salaire"]
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for data in data_to_load:
+            writer.writerow(data)
+
+def main():
+    data_to_transform = extract("input.csv")
+    data_to_load = transform(data_to_transform)
+    load(data_to_load, "output.csv")
 
 # Ne touchez pas le code ci-dessous
 if __name__ == "__main__":
