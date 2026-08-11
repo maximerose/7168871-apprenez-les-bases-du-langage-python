f300e0c1e7bceb667e594200d7be08d87c39599e
maximerose <maxime.r80@gmail.com>
2026-08-11 13:23:46 +0200

    Exercice P1C5

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,14 @@
-# Écrivez votre code ici !
+nom = "Maxime"
+age = 32
+taille = 1.74
+est_etudiant = False
+
+print(f"Nom: {nom}")
+print(f"Age: {age}")
+print(f"Taille: {taille}")
+print(f"Est étudiant: {est_etudiant}")
+
+print(f"Type nom: {type(nom)}")
+print(f"Type age: {type(age)}")
+print(f"Type taill: {type(taille)}")
+print(f"Type est_etudiant: {type(est_etudiant)}")
