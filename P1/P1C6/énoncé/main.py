cc2a0e1be83d1ad5072742b23da1129314eda336
maximerose <maxime.r80@gmail.com>
2026-08-11 13:33:30 +0200

    Exercice P1C6

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,19 @@
-# Écrivez votre code ici !
+from ctypes import sizeof
+
+
+fruits = ["pomme", "banane", "orange"]
+print(fruits)
+print("On ajoute kiwi")
+fruits.append("kiwi")
+print(fruits)
+print("On supprime orange")
+fruits.remove("orange")
+print(fruits)
+print("On change le 2eme en ananas")
+fruits[1] = "ananas"
+print(fruits)
+print(f"Longueur: {len(fruits)}")
+print("On trie la liste dans l'ordre alphabétique")
+fruits.sort()
+print(fruits)
+
