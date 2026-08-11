d40fbb496b09cf9a4d4802a5babfe7e3c7f9e6fb
maximerose <maxime.r80@gmail.com>
2026-08-11 13:41:20 +0200

    Exercice P1C7

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,14 @@
-# Écrivez votre code ici !
+fruits = {"pomme": "rouge", "banane": "jaune", "orange": "orange"}
+print(fruits)
+print("On ajoute kiwi : vert")
+fruits["kiwi"] = "vert"
+print(fruits)
+couleur_banane = fruits["banane"]
+print(f"couleur_banane: {couleur_banane}")
+print("La pomme devient verte")
+fruits["pomme"] = "vert"
+print(fruits)
+print("On supprime banane")
+del fruits["banane"]
+print(fruits)
+print(f"Clés restantes: {fruits.keys()}")
